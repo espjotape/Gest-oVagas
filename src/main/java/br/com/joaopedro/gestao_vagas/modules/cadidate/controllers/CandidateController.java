b813acba045c010e33f8e84f0d197be14dacf643
João Pedro Espindola <joaope.espindola2005@gmail.com>
2025-01-07 11:08:48 -0300

    Inserting validation

--- src/main/java/br/com/joaopedro/gestao_vagas/modules/cadidate/controllers/CandidateController.java
+++ src/main/java/br/com/joaopedro/gestao_vagas/modules/cadidate/controllers/CandidateController.java
@@ -7,13 +7,14 @@ import br.com.joaopedro.gestao_vagas.modules.cadidate.CandidateEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/candidate")
 public class CandidateController {
 
     @PostMapping("/")
-    public void create( @RequestBody CandidateEntity candidateEntity) {
+    public void create( @Valid @RequestBody CandidateEntity candidateEntity) {
         System.out.println("Candidato");
         System.out.println(candidateEntity.getEmail());
     }
