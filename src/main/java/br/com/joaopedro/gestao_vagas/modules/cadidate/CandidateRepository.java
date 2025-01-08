abe47bd1b1ab524f7de869199ac6a2a566028b34
João Pedro Espindola <joaope.espindola2005@gmail.com>
2025-01-08 13:27:51 -0300

    Validating existing user

--- src/main/java/br/com/joaopedro/gestao_vagas/modules/cadidate/CandidateRepository.java
+++ src/main/java/br/com/joaopedro/gestao_vagas/modules/cadidate/CandidateRepository.java
@@ -1,8 +1,9 @@
 package br.com.joaopedro.gestao_vagas.modules.cadidate;
 
+import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CandidateRepository extends JpaRepository<CandidateEntity, UUID> {
-    
+    Optional<CandidateEntity> findByUsernameOrEmail (String username, String email);
 }
--- src/main/java/br/com/joaopedro/gestao_vagas/modules/cadidate/controllers/CandidateController.java
+++ src/main/java/br/com/joaopedro/gestao_vagas/modules/cadidate/controllers/CandidateController.java
@@ -3,6 +3,7 @@ package br.com.joaopedro.gestao_vagas.modules.cadidate.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.joaopedro.gestao_vagas.exceptions.UserFoundException;
 import br.com.joaopedro.gestao_vagas.modules.cadidate.CandidateEntity;
 import br.com.joaopedro.gestao_vagas.modules.cadidate.CandidateRepository;
 
@@ -20,6 +21,12 @@ public class CandidateController {
 
     @PostMapping("/")
     public CandidateEntity create( @Valid @RequestBody CandidateEntity candidateEntity) {
-       return this.candidateRepository.save(candidateEntity);
+        this.candidateRepository
+            .findByUsernameOrEmail(candidateEntity.getUsername(), candidateEntity.getEmail())
+            .ifPresent((user) -> {
+                throw new UserFoundException();
+            });
+
+        return this.candidateRepository.save(candidateEntity);
     }
 }
