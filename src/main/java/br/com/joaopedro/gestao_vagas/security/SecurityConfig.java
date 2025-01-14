3aef2cc8e8a90159c9ca544c00cb84729ac66471
João Pedro Espindola <joaope.espindola2005@gmail.com>
2025-01-14 11:01:06 -0300

    Configuring public routes

--- src/main/java/br/com/joaopedro/gestao_vagas/security/SecurityConfig.java
+++ src/main/java/br/com/joaopedro/gestao_vagas/security/SecurityConfig.java
@@ -7,9 +7,16 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+ 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(csrf -> csrf.disable());
+        http.csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/candidate/**").permitAll()
+                        .requestMatchers("/company/**").permitAll()
+                        .anyRequest().authenticated()
+                );
+        
         return http.build();
     }
 }
