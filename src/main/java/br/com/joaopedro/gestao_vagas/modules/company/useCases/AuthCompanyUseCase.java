b2be87b7ee6ba8178d25aba2597281145109d8d8
João Pedro <joaope.espindola2005@gmail.com>
2025-03-24 11:11:40 -0300

    Add roles field in enterprise authentication response DTO

--- src/main/java/br/com/joaopedro/gestao_vagas/modules/company/useCases/AuthCompanyUseCase.java
+++ src/main/java/br/com/joaopedro/gestao_vagas/modules/company/useCases/AuthCompanyUseCase.java
@@ -52,12 +52,15 @@ public class AuthCompanyUseCase {
         var token = JWT.create()
             .withIssuer("javagas")
             .withSubject(company.getId().toString())
-            .withClaim("roles", Arrays.asList("ROLE_COMPANY"))
+            .withClaim("roles", Arrays.asList("COMPANY"))
             .sign(algorithm);
+
+        var roles = Arrays.asList("COMPANY");
 
         var authCompanyResponseDTO = AuthCompanyResponseDTO.builder()
             .access_token(token)
             .expires_in(expiresIn.toEpochMilli())
+            .roles(roles)
             .build();
 
         return authCompanyResponseDTO;
